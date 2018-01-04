fd8dc0b758ec5aeb238138c7e245271a1a261dc3
Lee Saxby <lsaxby@gresham-computing.com>
2018-01-04 08:42:25 +0000

    Update formatting

--- src/pages/Grid/Filters.js
+++ src/pages/Grid/Filters.js
@@ -3,68 +3,66 @@ import React, {PureComponent} from 'react';
 import { ControlLabel, FormControl, FormGroup, Panel } from 'react-bootstrap';
 
 export default class GridTable extends PureComponent {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
             name: '',
             age: '',
             random: '',
             date: ''
-    };
-  }
+        };
+    }
 
-  static propTypes = {
-      onFilterChange: PropTypes.func.isRequired
-  }
+    static propTypes = {
+        onFilterChange: PropTypes.func.isRequired
+    }
 
-  render() {
-      return (
-          <Panel header="Filters" bsStyle="primary">
-              <FormGroup>
-                  <ControlLabel>Name</ControlLabel>
-                  <FormControl id="search"
-                               name="name"
-                               type="text"
-                               placeholder="Filter..."
-                               value={this.state.name}
-                               onChange={this._onFilterChange} />
-              </FormGroup>
-              <FormGroup>
-                  <ControlLabel>Age</ControlLabel>
-                  <FormControl id="search"
-                              name="age"
-                              type="text"
-                              placeholder="Filter..."
-                              value={this.state.age}
-                              onChange={this._onFilterChange} />
-              </FormGroup>
-              <FormGroup>
-                  <ControlLabel>Description</ControlLabel>
-                  <FormControl id="search"
-                               name="random"
-                               type="text"
-                               placeholder="Filter..."
-                               value={this.state.random}
-                               onChange={this._onFilterChange} />
-              </FormGroup>
-              <FormGroup>
-                  <ControlLabel>Date</ControlLabel>
-                  <FormControl id="search"
-                               name="date"
-                               type="date"
-                               placeholder="Filter..."
-                               value={this.state.date}
-                               onChange={this._onFilterChange} />
-                  </FormGroup>
-          </Panel>
-      );
-  }
+    render() {
+        return (
+            <Panel header="Filters" bsStyle="primary">
+                <FormGroup>
+                    <ControlLabel>Name</ControlLabel>
+                    <FormControl id="search"
+                                name="name"
+                                type="text"
+                                placeholder="Filter..."
+                                value={this.state.name}
+                                onChange={this._onFilterChange} />
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Age</ControlLabel>
+                    <FormControl id="search"
+                                name="age"
+                                type="text"
+                                placeholder="Filter..."
+                                value={this.state.age}
+                                onChange={this._onFilterChange} />
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Description</ControlLabel>
+                    <FormControl id="search"
+                                name="random"
+                                type="text"
+                                placeholder="Filter..."
+                                value={this.state.random}
+                                onChange={this._onFilterChange} />
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Date</ControlLabel>
+                    <FormControl id="search"
+                                name="date"
+                                type="date"
+                                placeholder="Filter..."
+                                value={this.state.date}
+                                onChange={this._onFilterChange} />
+                    </FormGroup>
+            </Panel>
+        );
+    }
 
-  _onFilterChange = (e) => {
-       const updatedFilters = Object.assign({}, this.state, { [e.target.name]: e.target.value });
-       this.setState(updatedFilters);
-       this.props.onFilterChange(e.target.name, e.target.value);
-  }
-
+    _onFilterChange = (e) => {
+        const updatedFilters = Object.assign({}, this.state, { [e.target.name]: e.target.value });
+        this.setState(updatedFilters);
+        this.props.onFilterChange(e.target.name, e.target.value);
+    }
 }
