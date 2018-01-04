375471d8219e9eae40d03a9ee96a39353cac3dff
Lee Saxby <lsaxby@gresham-computing.com>
2018-01-04 13:48:17 +0000

    Move Grid to separate component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Immutable from 'immutable';
-import {generateRandomList} from './utils';
-
 import Cube from './pages/Cube';
-
-const list = Immutable.List(generateRandomList());
 
 export function App() {
     return (
-        <Cube list={list} />
+        <Cube />
     );
 }
--- src/pages/Cube.js
+++ src/pages/Cube.js
@@ -1,57 +1,22 @@
-import PropTypes from 'prop-types';
 import React, {PureComponent} from 'react';
 import Immutable from 'immutable';
-import { ToggleButtonGroup, ToggleButton, Grid as GridStyle, Row, Col, FormGroup, Panel, Radio} from 'react-bootstrap';
-import { Grid, AutoSizer } from 'react-virtualized';
+import { Grid as GridStyle, Row, Col, Panel } from 'react-bootstrap';
+import GridTable from './Cube/GridTable';
 import Filter from './Cube/Filter';
 import Sort from './Cube/Sort';
 
-import cn from 'classnames';
-import styles from './Cube/Cube.css';
+import {generateRandomList} from '../utils';
 
-
+const list = Immutable.List(generateRandomList());
 
 const SortDirection = {ASC: 'ASC', DESC: 'DESC'};
 
 export default class Cube extends PureComponent {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      sort: {
-        sortDirection: 'ASC',
-        sortBy: 'name'
-      },
-      list: props.list,
-      columnCount: 1000,
-      height: 600,
-      overscanColumnCount: 0,
-      overscanRowCount: 10,
-      rowHeight: 40,
-      rowCount: 1000000,
-      scrollToColumn: undefined,
-      scrollToRow: 0,
-      useDynamicRowHeight: false,
-    };
-
-  }
-
-  static propTypes = {
-      list: PropTypes.instanceOf(Immutable.List).isRequired
   }
 
   render() {
-    const {
-      columnCount,
-      height,
-      overscanColumnCount,
-      overscanRowCount,
-      rowHeight,
-      rowCount,
-      scrollToColumn,
-      scrollToRow,
-      useDynamicRowHeight,
-    } = this.state;
 
     return (
 
@@ -75,130 +40,13 @@ export default class Cube extends PureComponent {
                 <Row>
                     <Col lg={12}>
                         <Panel header="Grid" bsStyle="primary" className="data-grid">
-                            <AutoSizer disableHeight>
-                              {({width}) => (
-                                  <Grid
-                                    cellRenderer={this._cellRenderer}
-                                    className={styles.BodyGrid}
-                                    columnWidth={this._getColumnWidth}
-                                    columnCount={columnCount}
-                                    height={height}
-                                    noContentRenderer={this._noContentRenderer}
-                                    overscanColumnCount={overscanColumnCount}
-                                    overscanRowCount={overscanRowCount}
-                                    rowHeight={useDynamicRowHeight ? this._getRowHeight : rowHeight}
-                                    rowCount={rowCount}
-                                    scrollToColumn={scrollToColumn}
-                                    scrollToRow={scrollToRow}
-                                    width={width}
-                                  />
-                              )}
-                            </AutoSizer>
+                            <GridTable list={list}/>
                         </Panel>
                     </Col>
                 </Row>
             </GridStyle>
         </div>
 
-    );
-  }
-
-  _cellRenderer = ({columnIndex, key, rowIndex, style}) => {
-      return this._renderBodyCell({columnIndex, key, rowIndex, style});
-  }
-
-  _onColummSelect = (index) => {
-    const { sortDirection } = this.state.sort;
-    const selectedColName = Object.keys(this.state.list.get(0))[index];
-    this.setState({
-      sort: {
-        sortDirection,
-        sortBy: selectedColName
-      }
-    });
-  }
-
-  _getColumnWidth = ({index}) => {
-    switch (index) {
-      case 0:
-        return 70;
-      case 1:
-        return 150;
-      case 2:
-        return 150;
-      case 3:
-        return 50;
-      case 4:
-        return 100;
-      default:
-        return 300;
-    }
-  }
-
-  _getDatum = (index) => {
-    const {list} = this.state;
-    return list.get(index % list.size);
-  }
-
-  _getRowClassName = (row) => {
-    return row % 2 === 0 ? styles.evenRow : styles.oddRow;
-  }
-
-  _getRowHeight = ({index}) => {
-    return this._getDatum(index).size;
-  }
-
-  _noContentRenderer = () => {
-    return <div className={styles.noCells}>No cells</div>;
-  }
-
-  _renderBodyCell = ({columnIndex, key, rowIndex, style}) => {
-    const rowClass = this._getRowClassName(rowIndex);
-    const datum = this._getDatum(rowIndex);
-
-    let content;
-
-    switch (columnIndex) {
-      case 0:
-        content = datum.index;
-        break;
-      case 1:
-        content = datum.name;
-        break;
-      case 2:
-        content = datum.otherNames;
-        break;
-      case 3:
-        content = datum.age;
-        break;
-      case 4:
-        content = datum.date;
-        break;
-      case 5:
-        content = datum.random2;
-        break;
-      case 6:
-        content = datum.random3;
-        break;
-      case 7:
-        content = datum.random4;
-        break;
-      case 8:
-        content = datum.random5;
-        break;
-      default:
-        content = datum.random;
-        break;
-    }
-
-    const classNames = cn(rowClass, styles.cell, {
-      [styles.centeredCell]: columnIndex > 2,
-    });
-
-    return (
-      <div onClick={() => {this._onColummSelect(columnIndex);}} className={classNames} key={key} style={style}>
-        {content}
-      </div>
     );
   }
 
