dc84b3d531091ded52c0221d1b3efdb0f248156d
Lee Saxby <lsaxby@gresham-computing.com>
2018-01-18 11:58:30 +0000

    Add basic debounce for filter inputs

--- src/pages/Cube/Filter.js
+++ src/pages/Cube/Filter.js
@@ -3,33 +3,55 @@ import PropTypes from 'prop-types';
 import { Map } from 'immutable';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-Filter.propTypes = {
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    onFilterChange: PropTypes.func.isRequired
-};
-
-
-export default function Filter(props) {
-    const onChange = (e) => {
-        props.onFilterChange(
-            Map({
-                name: props.name,
-                type: props.type,
-                value: e.target.value,
-            })
-        );
+export default class Filter extends React.Component {
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+        onFilterChange: PropTypes.func.isRequired
     };
 
-    return (
-        <FormGroup>
-            <ControlLabel>{props.name}</ControlLabel>
-                <FormControl name={props.name}
-                             type={props.type}
-                             placeholder="Filter..."
-                             value={props.value}
-                             onChange={onChange} />
-        </FormGroup>
-    );
+    constructor (props) {
+        super(props);
+        this.timeout;
+        this.state = {
+            value: this.props.value
+        };
+    }
+
+    updateFilter = (value) => {
+        this.props.onFilterChange(
+            Map({
+                name: this.props.name,
+                type: this.props.type,
+                value: value,
+            })
+        );
+
+    };
+
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value
+        });
+
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout((value) => {
+            this.updateFilter(value);
+        }, 500, e.target.value);
+    };
+
+    render() {
+        return (
+            <FormGroup>
+                <ControlLabel>{this.props.name}</ControlLabel>
+                    <FormControl name={this.props.name}
+                                 type={this.props.type}
+                                 placeholder="Filter..."
+                                 value={this.state.value}
+                                 onChange={this.onChange} />
+            </FormGroup>
+        );
+    }
+
 }
