ec8876194c3c43388a83746d11c1502781352e6a
Lee Saxby <lsaxby@gresham-computing.com>
2018-01-19 09:31:23 +0000

    Add endpoint for list and total

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const generateRandomList = require('./utils.js');
 const app  = express();
-const list = generateRandomList();
+const data = generateRandomList();
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9999');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     next();
 });
 
-app.get('/',function(req,res){
-    res.json(list);
+app.get('/list',function(req,res){
+    res.json({ list: data.list });
+});
+
+app.get('/total',function(req,res){
+    res.json({ total: data.totalCount });
 });
 
 app.listen(3000, () => {
